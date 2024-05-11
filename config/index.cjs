e4bd4a5014fe04c16f7560c0798cb772ecdea4dd
Liu Jiahao <78532162+jiahaonb@users.noreply.github.com>
2024-05-11 14:09:04 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oZFEf6zjppqPfsat9ZK6OsA4Bp5M',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'MfQkENkgZbgSIQBPbgGwoBMmxlwo1YCNVgHLI-dMWEA',
+      useTemplateId: '-3RwVWhtdFJ2zyJ3Djrt47tVM7StYqChaxr9pgrsmeQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '04-24',
       festivals: [
